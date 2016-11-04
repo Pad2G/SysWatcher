cac307d4c2a67a3490bc1727f9daf45f1b50ba78
Pad2G <pad2g.vb@gmail.com>
2016-11-04 20:31:40 +0100

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Watcher2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.IncludeSubdirectories = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
+            this.fileSystemWatcher1.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            this.fileSystemWatcher1.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher1_Renamed);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 245);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(670, 178);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(574, 222);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Nascondi dettagli";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(570, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Inserire Directory manualmente o premere Sfoglia:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(607, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Sfoglia";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Pad2Monitor";
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 447);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pad2Monitor 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Watcher2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string finalPath="";
+        static string exePath = "";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                String p = Environment.GetEnvironmentVariable("watchpath", EnvironmentVariableTarget.Machine);
+                if (string.IsNullOrEmpty(p))         // ADD SCHEDULED TASK IF NOT PRESENT
+                {
+                    string exePath = System.Windows.Forms.Application.ExecutablePath;
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.Arguments = "/C schtasks /create /sc onlogon /tn watcher /rl highest /tr \"" + exePath + "\"";
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+            catch ( Exception ex)
+            {
+                MessageBox.Show("Errore nell'accesso al registro", "Errore", MessageBoxButtons.OK);
+            }
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+            richTextBox1.Text += "\nChanged: " + e.FullPath;
+        }
+        private void move(string inp, string outp) // RENAME FILE
+        {
+            bool copied = false;
+            int i = 0;
+            do {
+                try
+                {
+                    File.Move(inp, outp);
+                    copied = true;
+                }
+                catch (Exception ex)
+                {
+                    /*if ((i % 1000 == 0) && (i != 0))  VERBOSE MODE
+                    {
+                        richTextBox1.Text += "\nTentativi correnti: "+i;
+                        DialogResult opts = MessageBox.Show("Windows impiega troppo a rispondere...Riprovare o interrompere?\n" + ex.ToString(), "Errore", MessageBoxButtons.RetryCancel);
+                        if (opts == DialogResult.Cancel)
+                        {
+                            richTextBox1.Text += "\nProcesso interrotto.";
+                            break;
+                        }
+                        else {
+                            i++;
+                            continue;
+                        }
+                    }*/
+                    copied = false;
+                    i++;
+                }
+             } while (!copied);
+        }
+        private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
+        {
+            int count = 0;
+            richTextBox1.Text += "\nCreated: " + e.FullPath;
+            Regex rgx = new Regex(@"(([012]\d)|3[01])-((0\d)|(1[012]))-\d{4} \d{2,2}-\d{2,2}-\d{2,2}$");  // check for date at the end of file
+            Match a = rgx.Match(e.FullPath.Split('.')[0]);
+            if (!a.Success)
+            {
+                DateTime now = new DateTime();
+                now = DateTime.Now;
+                int index = e.FullPath.IndexOf(".");
+                finalPath = e.FullPath.Substring(0, index) + " " + now.ToString().Replace('/', '-').Replace(':', '-') + e.FullPath.Substring(e.FullPath.IndexOf('.'));
+                exePath = e.FullPath;
+                richTextBox1.Text += "\nTentativi di copia in corso...";
+                count++;
+                BackgroundWorker worker = new BackgroundWorker();
+                worker.DoWork += Worker_DoWork;
+                worker.RunWorkerAsync(count);
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            move(exePath, finalPath);
+        }
+
+        private void fileSystemWatcher1_Deleted(object sender, FileSystemEventArgs e)
+        {
+            richTextBox1.Text += "\nDeleted: " + e.FullPath;
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, RenamedEventArgs e)
+        {
+            richTextBox1.Text += "\nRenamed: " + e.OldFullPath + " -> " + e.FullPath;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.Checked)
+            {
+                richTextBox1.Hide();
+            } else
+            {
+                richTextBox1.Show();
+                cb.Text = "Nascondi dettagli";
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            if (button1.Text == "Sfoglia")
+            {
+                button1.Text = "Modifica";
+            }
+            label1.Text = "Premere OK per iniziare";
+            if (!string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath)) {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            } else {
+                MessageBox.Show("Selezionare un vero percorso");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Directory.Exists(textBox1.Text))
+            {
+                richTextBox1.Text += "\nImpostazione variabile d'ambiente...";
+                fileSystemWatcher1.Path = textBox1.Text;
+                Environment.SetEnvironmentVariable("watchpath", textBox1.Text,EnvironmentVariableTarget.Machine); // installed
+                richTextBox1.Text += "\nIn esecuzione su cartella: "+textBox1.Text+"\n";
+                label1.Text = "Inserire Directory manualmente o premere Modifica:";
+            }
+            else
+            {
+                MessageBox.Show("Directory non esistente.", "Errore", MessageBoxButtons.OK);
+
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();     
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            String p = Environment.GetEnvironmentVariable("watchpath", EnvironmentVariableTarget.Machine);
+            if (!string.IsNullOrEmpty(p))
+            {
+                fileSystemWatcher1.Path = p; // start listening
+                textBox1.Text = p;
+                richTextBox1.Text += "\nIn esecuzione su cartella: "+p+"\n";
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon1.Visible = false;
+        }
+    }
+}
